e9928a4e6311919628afb52e745df8be3da95501
Anastasia Nikiforova <nastasya.nikiforova@gmail.com>
2021-01-02 20:39:33 +0300

    add solution for 04-SPA 05-AuthPages

--- 04-SPA/05-AuthPages/router/index.js
+++ 04-SPA/05-AuthPages/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import IndexPage from '../views/IndexPage';
+import LoginPage from '../views/LoginPage';
+import RegisterPage from '../views/RegisterPage';
 
 Vue.use(VueRouter);
 
@@ -11,6 +13,14 @@ export const router = new VueRouter({
     {
       path: '/',
       component: IndexPage,
+    },
+    {
+      path: '/login',
+      component: LoginPage,
+    },
+    {
+      path: '/register',
+      component: RegisterPage,
     },
   ],
 });
