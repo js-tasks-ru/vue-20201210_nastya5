3a07164cb48b60595da140793bbcb19fa0764f63
Anastasia Nikiforova <nastasya.nikiforova@gmail.com>
2021-01-09 00:50:18 +0300

    add solution for 06-deep-vue 01-Buttons

--- 06-deep-vue/01-Buttons/components/BaseButton.vue
+++ 06-deep-vue/01-Buttons/components/BaseButton.vue
@@ -1,10 +1,49 @@
+<!--
 <template>
-  <button class="button"></button>
-</template>
+  <button
+    class="button"
+    :class="{ button_block: block }"
+    v-bind="$attrs"
+    v-on="$listeners"
+    :href="$attrs['href']"
+    :target="$attrs['target']"
+  >
+    <slot />
+  </button>
+</template>-->
 
 <script>
+
 export default {
   name: 'BaseButton',
+
+  inheritAttrs: false,
+
+  props: {
+    block: {
+      type: Boolean,
+    },
+    tag: {
+      type: String,
+      default: 'button',
+      validator: (value) => ['button', 'a', 'router-link'].includes(value),
+    },
+  },
+
+  render(h) {
+    return h(
+      this.tag==='router-link'? 'a' : this.tag,
+      {
+        class: {
+          button: true,
+          button_block: this.block,
+        },
+        on: this.$listeners,
+        attrs: {... this.$attrs, href: this.tag==='router-link'? this.$attrs['to'] : this.$attrs['href'] },
+      },
+      this.$slots.default,
+    );
+  },
 };
 </script>
 
