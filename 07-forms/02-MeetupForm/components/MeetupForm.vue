aa1861179f976b65df537c6022e2d55b7b9e9b0d
Anastasia Nikiforova <nastasya.nikiforova@gmail.com>
2021-01-11 00:12:38 +0300

    add solution for 07-forms 02-MeetupForm

--- 07-forms/02-MeetupForm/components/MeetupForm.vue
+++ 07-forms/02-MeetupForm/components/MeetupForm.vue
@@ -1,34 +1,50 @@
 <template>
-  <form class="form meetup-form">
+  <form class="form meetup-form" @submit="handleSubmit">
     <div class="meetup-form__content">
       <fieldset class="form-section">
         <div class="form-group">
           <label class="form-label">Название</label>
-          <input class="form-control" />
+          <input class="form-control" v-model="meetup_.title" />
         </div>
         <div class="form-group">
           <label class="form-label">Дата</label>
-          <input class="form-control" type="date" />
+          <input
+            class="form-control"
+            type="date"
+            :value="dateOnlyString"
+            @input="meetup_.date = $event.target.value"
+          />
         </div>
         <div class="form-group">
           <label class="form-label">Место</label>
-          <input class="form-control" />
+          <input class="form-control" v-model="meetup_.place" />
         </div>
         <div class="form-group">
           <label class="form-label">Описание</label>
-          <textarea class="form-control" rows="3"></textarea>
+          <textarea
+            class="form-control"
+            rows="3"
+            v-model="meetup_.description"
+          ></textarea>
         </div>
         <div class="form-group">
           <label class="form-label">Изображение</label>
-          <image-uploader />
+          <image-uploader v-model="meetup_.imageId" />
         </div>
       </fieldset>
 
       <h3 class="form__section-title">Программа</h3>
-      <!-- <meetup-agenda-item-form class="mb-3" />-->
+      <meetup-agenda-item-form
+        v-for="(agendaItem, idx) in meetup_.agenda"
+        :key="agendaItem.id"
+        :agendaItem="agendaItem"
+        @update:agendaItem="updateAgendaItem(idx, $event)"
+        @remove="removeAgendaItem(idx)"
+        class="mb-3"
+      />
 
       <div class="form-section_append">
-        <button type="button" data-test="addAgendaItem">
+        <button type="button" data-test="addAgendaItem" @click="addAgendaItem">
           + Добавить этап программы
         </button>
       </div>
@@ -40,6 +56,7 @@
           class="button button_secondary button_block"
           type="button"
           data-test="cancel"
+          @click="handleCancel"
         >
           Отмена
         </button>
@@ -48,7 +65,7 @@
           type="submit"
           data-test="submit"
         >
-          Submit
+          {{ submitText }}
         </button>
       </div>
     </div>
@@ -72,12 +89,73 @@ function buildAgendaItem() {
   };
 }
 
+function deepClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+const getDateOnlyString = (date) => {
+  const YYYY = date.getUTCFullYear();
+  const MM = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+  const DD = date.getUTCDate().toString().padStart(2, '0');
+  return `${YYYY}-${MM}-${DD}`;
+};
+
 export default {
   name: 'MeetupForm',
 
   components: {
     ImageUploader,
     MeetupAgendaItemForm,
+  },
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    },
+    submitText: {
+      type: String,
+      defaultValue: 'Submit',
+    },
+  },
+
+  data() {
+    return {
+      meetup_: deepClone(this.meetup),
+    };
+  },
+
+  computed: {
+    dateOnlyString() {
+      return getDateOnlyString(new Date(this.meetup_.date));
+    },
+  },
+
+  methods: {
+    handleSubmit() {
+      this.$emit('submit', deepClone(this.meetup_));
+    },
+
+    handleCancel() {
+      this.$emit('cancel');
+    },
+
+    addAgendaItem() {
+      const newAgendaItem = buildAgendaItem();
+      if(this.meetup_.agenda.length){
+        newAgendaItem.startsAt = this.meetup_.agenda[this.meetup_.agenda.length-1].endsAt;
+        newAgendaItem.endsAt = newAgendaItem.startsAt;
+      }
+      this.meetup_.agenda.push(newAgendaItem);
+    },
+
+    updateAgendaItem(idx, newAgendaItem) {
+      this.meetup_.agenda.splice(idx, 1, newAgendaItem);
+    },
+
+    removeAgendaItem(idx) {
+      this.meetup_.agenda.splice(idx, 1);
+    },
   },
 };
 </script>
